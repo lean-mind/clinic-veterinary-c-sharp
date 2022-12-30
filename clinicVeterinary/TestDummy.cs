32f2ceb03f386faef84e90fe08d0b72eec23a510
Rubén Zagon <ruben.zagon@gmail.com>
2022-12-30 13:24:56 +0000

    🧪 add dummy test

---
+++ clinicVeterinary/TestDummy.cs
@@ -0,0 +1,14 @@
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace clinicVeterinary;
+
+public class TestDummy
+{
+    [Test]
+    public void test_name()
+    {
+        string actual = "ABCDEFGHI";
+        actual.Should().StartWith("AB").And.EndWith("HI").And.Contain("EF").And.HaveLength(9);
+    }
+}
