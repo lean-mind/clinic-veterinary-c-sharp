7752469e50680579938720ec50e1f5c1ae1f95d5
jonayGodoyLeanmind <103043182+jonayGodoyLeanmind@users.noreply.github.com>
2023-01-14 13:38:19 +0100

    just add new koan

--- test/Koans/Common/GeneralKoans.cs
+++ test/Koans/Common/GeneralKoans.cs
@@ -2,5 +2,30 @@ namespace test.Koans.Common;
 
 public class GeneralKoans
 {
+    [Test]
+    public void named_parameters()
+    {
+        var car1 = new Car("name", "brand");
+        var car2 = new Car(name:"name", brand: "brand");
+
+        car1.Brand.Should().Be("brand");
+        car2.Brand.Should().Be("brand");
+        car1.Name.Should().Be("name");
+        car2.Name.Should().Be("name");
+    }
     
+}
+
+
+public class Car
+{
+    public string Brand { get; set; }
+
+    public string Name { get; set; }
+
+    public Car(string brand, string name)
+    {
+        Brand = brand;
+        Name = name;
+    }
 }
