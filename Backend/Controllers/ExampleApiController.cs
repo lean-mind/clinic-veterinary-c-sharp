2cadcaf253a16f3eb4017c32634daa156e1d524d
jonayGodoyLeanmind <103043182+jonayGodoyLeanmind@users.noreply.github.com>
2023-01-16 15:02:56 +0100

    refactor: rename

--- Backend/Controllers/ExampleApiController.cs
+++ Backend/Controllers/ExampleApiController.cs
@@ -4,16 +4,16 @@ namespace Backend.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class WeatherForecastController : ControllerBase
+public class ExampleApiController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-    private readonly ILogger<WeatherForecastController> _logger;
+    private readonly ILogger<ExampleApiController> _logger;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    public ExampleApiController(ILogger<ExampleApiController> logger)
     {
         _logger = logger;
     }
