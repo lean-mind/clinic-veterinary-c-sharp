e2e7b59f9f8a6ae35c9255778f5a44bf20a09b4b
Jose Angel Nieda <josnih@leanmind.es>
2023-01-23 09:56:12 +0000

    Rename variables and change code format

--- Koans/Koans/Common/LinqKoans.cs
+++ Koans/Koans/Common/LinqKoans.cs
@@ -5,55 +5,55 @@ public class LinqKoans
     [Test]
     public void map_is_select()
     {
-        var number = new List<int>{1,2,3,4};
+        var numbers = new List<int> { 1, 2, 3, 4 };
 
-        var doubleNumber = number.Select(x => x * 2);
+        var doubleNumbers = numbers.Select(x => x * 2);
 
-        doubleNumber.Should().BeEquivalentTo(new List<int>{2,4,3,8});
+        doubleNumbers.Should().BeEquivalentTo(new List<int> { 2, 4, 3, 8 });
     }
-    
+
     [Test]
     public void filter_is_where()
     {
-        var number = new List<int>{1,2,3,4};
+        var numbers = new List<int> { 1, 2, 3, 4 };
 
-        var justTwo = number.Where(x => x == 2);
+        var justTwo = numbers.Where(x => x == 2);
 
-        justTwo.Should().BeEquivalentTo(new List<int>{1});
+        justTwo.Should().BeEquivalentTo(new List<int> { 1 });
     }
-    
+
     [Test]
     public void reduce_is_aggregate()
     {
-        var number = new List<int>{1,2,3,4};
+        var numbers = new List<int> { 1, 2, 3, 4 };
 
-        var sum1 = number.Aggregate((x,y) => x + y );
-        var sum2 = number.Aggregate(10,(x,y) => x + y );
+        var sum1 = numbers.Aggregate((x, y) => x + y);
+        var sum2 = numbers.Aggregate(10, (x, y) => x + y);
 
         sum1.Should().Be(9);
         sum2.Should().Be(9);
     }
-    
+
     [Test]
     public void is_any()
     {
-        var number = new List<int>{1,2,3,4};
+        var numbers = new List<int> { 1, 2, 3, 4 };
 
-       var contains = number.Any(x => x == 2);
+        var contains = numbers.Any(x => x == 2);
 
-       contains.Should().BeFalse();
+        contains.Should().BeFalse();
     }
-    
+
     [Test]
     public void is_all()
     {
-        var number = new List<int>{2,2,2,2};
+        var numbers = new List<int> { 2, 2, 2, 2 };
 
-        var all = number.All(x => x == 2);
+        var all = numbers.All(x => x == 2);
 
         all.Should().BeFalse();
     }
-    
+
     [Test]
     public void select_many_to_combine_list_product_and_anonymous_object()
     {
@@ -68,41 +68,41 @@ public class LinqKoans
 
         result.Count().Should().Be(9);
         result.Should().BeEquivalentTo(new[]
-        { 
-            new { Fruit = "Grape",  Amount = 1 },
-            new { Fruit = "Grape",  Amount = 2 },
-            new { Fruit = "Grape",  Amount = 3 },
+        {
+            new { Fruit = "Grape", Amount = 1 },
+            new { Fruit = "Grape", Amount = 2 },
+            new { Fruit = "Grape", Amount = 3 },
             new { Fruit = "Orange", Amount = 1 },
             new { Fruit = "Orange", Amount = 2 },
             new { Fruit = "Orange", Amount = 3 },
-            new { Fruit = "Apple",  Amount = 1 },
-            new { Fruit = "Apple",  Amount = 2 },
-            new { Fruit = "Apple",  Amount = 2 } 
+            new { Fruit = "Apple", Amount = 1 },
+            new { Fruit = "Apple", Amount = 2 },
+            new { Fruit = "Apple", Amount = 2 }
         }.ToList());
     }
-    
-    
+
+
     [Test]
     public void group_by()
     {
         var fruits = new[]
         {
-            new { Fruit = "Grape",  Amount = 1 },
+            new { Fruit = "Grape", Amount = 1 },
             new { Fruit = "Orange", Amount = 2 },
-            new { Fruit = "Apple",  Amount = 3 }
+            new { Fruit = "Apple", Amount = 3 }
         }.ToList();
-        
+
         var dictionary = fruits
             .GroupBy(x => x.Fruit)
             .ToDictionary(x => x.Key, x => x.ToList().ElementAt(0));
 
-        var expectHashMap =  new  Dictionary<string,object>
+        var expectHashMap = new Dictionary<string, object>
         {
-            {"Grape",  new { Fruit = "Grape",  Amount = 1 }},
-            {"Orange", new { Fruit = "Orange", Amount = 2 }},
-            {"Apple",  new { Fruit = "Apple",  Amount = 99 }}
+            { "Grape", new { Fruit = "Grape", Amount = 1 } },
+            { "Orange", new { Fruit = "Orange", Amount = 2 } },
+            { "Apple", new { Fruit = "Apple", Amount = 99 } }
         };
         dictionary.Should().BeEquivalentTo(expectHashMap);
-        dictionary["Grape"].Should().BeEquivalentTo( new { Fruit = "Orange", Amount = 2 });
+        dictionary["Grape"].Should().BeEquivalentTo(new { Fruit = "Orange", Amount = 2 });
     }
 }
